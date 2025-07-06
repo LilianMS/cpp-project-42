9c263fb8db9828f2dd5b25da4390e16aa8ddea95
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2025-07-05 22:10:59 -0300

    :sparkles: feat: adds module_01/(ex05, ex06)

---
+++ module_01/ex05/Harl.cpp
@@ -0,0 +1,36 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+Harl::~Harl() {}
+
+void Harl::debug(void) {
+	std::cout << "[DEBUG] I love having extra bacon on my burger!" << std::endl;
+}
+
+void Harl::info(void) {
+	std::cout << "[INFO] Adding bacon costs more money." << std::endl;
+}
+
+void Harl::warning(void) {
+	std::cout << "[WARNING] I think I deserve some free bacon." << std::endl;
+}
+
+void Harl::error(void) {
+	std::cout << "[ERROR] This is unacceptable! I want to speak to the manager!" << std::endl;
+}
+
+void Harl::complain(std::string level) {
+	typedef void (Harl::*HarlMethod)(); // define tipo de ponteiro para método
+
+	std::string levels[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+	HarlMethod methods[4] = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+
+	for (int i = 0; i < 4; i++) {
+		if (level == levels[i]) {
+			(this->*methods[i])();
+			return ;
+		}
+	}
+
+	std::cout << "[UNKNOWN] Invalid complaint level: " << level << std::endl;
+}
