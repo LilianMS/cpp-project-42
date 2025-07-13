647a6cd2d8fcadc4dc0b313d19e60b91dd315ed2
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2025-07-13 20:08:15 -0300

    :sparkles: fix:  organizes module_00
                    - organizes directories and makefile in module_00

--- module_00/ex00/Megaphone.cpp
+++ module_00/ex00/Megaphone.cpp
@@ -3,30 +3,37 @@
 Megaphone::Megaphone() {}
 Megaphone::~Megaphone() {}
 
-void Megaphone::project(int argc, char **argv) const {
-	if (argc < 2) {
+void Megaphone::project(int argc, char **argv) const
+{
+	if (argc < 2)
+	{
 		noArg();
-		return ;
+		return;
 	}
 	std::cout << "📣 ";
-	for (int i = 1; i < argc; ++i) {
+	for (int i = 1; i < argc; ++i)
+	{
 		std::string str(argv[i]);
 		toUpperCase(str);
 		std::cout << str;
-		if (i < argc - 1) {
+		if (i < argc - 1)
+		{
 			std::cout << " ";
 		}
 	}
 	std::cout << std::endl;
 }
 
-void Megaphone::toUpperCase(std::string &str) const {
-	for (size_t i = 0; i < str.length(); ++i) {
+void Megaphone::toUpperCase(std::string &str) const
+{
+	for (size_t i = 0; i < str.length(); ++i)
+	{
 		str[i] = std::toupper(static_cast<unsigned char>(str[i]));
-    }
+	}
 }
 
-void Megaphone::noArg() const {
+void Megaphone::noArg() const
+{
 	std::cout << "📣 ";
 	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 }
